42c38c86aae25d6176d377366d95bc2e4b8f0c28
Daniel P Landon <dougedd@yahoo.com>
2019-12-16 14:50:48 -0800

    ready to submit for assignment

---
+++ server.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const MOVIES = require('./movies-data-small.json')
+
+console.log(process.env.API_TOKEN)
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    console.log('validate bearer token middleware')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    
+    
+    // move to the next middleware
+    next()
+})
+
+
+app.get('/movies', function handleGetGenre(req, res) {
+    let response = MOVIES;
+
+
+// filter our MOVIES by genre if genre query param is present
+
+if(req.query.genre) {
+    response = response.filter(movies => 
+        // case insensitive searching
+        movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+}
+
+if(req.query.country) {
+    response = response.filter(movies => 
+        // case insensitive searching
+        movies.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+}
+
+if(req.query.avg_vote) {
+    response = response.filter(movies => 
+        // case insensitive searching
+        movies.avg_vote >= (req.query.avg_vote)
+    )
+}
+
+
+
+
+
+res.json(response)
+
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
